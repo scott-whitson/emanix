476b9cbd61fd4cafd0164bfcf7f28f290088eff9
Scott Whitson <scott@scottwhitson.com>
2026-07-15 14:44:25 -0400

    fix: move disko config inline in zord nixos config

--- hosts/zord/configuration.nix
+++ hosts/zord/configuration.nix
@@ -7,8 +7,44 @@
     ../../modules/nixos/hardware/thinkpad-t14-gen5-amd.nix
     ../../modules/nixos/ewm.nix
     ../../modules/nixos/desktop.nix
-    ./disko.nix
   ];
+
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "fmask=0077" "dmask=0077" ];
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "luks";
+            name = "cryptroot";
+            settings.allowDiscards = true;
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "@" = { mountpoint = "/"; mountOptions = [ "compress=zstd" ]; };
+                "@nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                "@home" = { mountpoint = "/home"; mountOptions = [ "compress=zstd" ]; };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 
   programs.zsh.enable = true;
 
